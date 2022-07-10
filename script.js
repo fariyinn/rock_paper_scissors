aba8da3e22cee7af38d77418cc0796ebe3462c6e
fariyinn <fariyinn@gmail.com>
2022-07-10 16:51:08 -0400

    Add eventListener to buttons, input to playRound

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection;
+let computerSelection;
 
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
     let randomNumber = Math.floor(Math.random() * options.length);
-    let computerSelection = options[randomNumber];
+    computerSelection = options[randomNumber];
     return computerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'paper') {
         computerScore++;
-        return ('You lose! Paper beats Rock!');
+        console.log('You lose! Paper beats Rock!');
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
         playerScore++;
-        return ('You win! Rock beats Scissors!');
+        console.log('You win! Rock beats Scissors!');
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
         playerScore++;
-        return ('You win! Paper beats Rock!');
+        console.log('You win! Paper beats Rock!');
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
         computerScore++;
-        return ('You lose! Scissors beats Paper!');
+        console.log('You lose! Scissors beats Paper!');
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         computerScore++;
-        return ('You lose! Rock beats Scissors!');
+        console.log('You lose! Rock beats Scissors!');
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         playerScore++;
-        return ('You win! Scissors beats Paper!');
+        console.log('You win! Scissors beats Paper!');
     } else if (playerSelection == computerSelection) {
-        return ('It\'s a tie! Play again!');
+        console.log('It\'s a tie! Play again!');
     } else {
-        return('That is not one of the 3 options!');
+        console.log('That is not one of the 3 options!');
     }
 }
 
-game();
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id, computerPlay());
+    });
+});
